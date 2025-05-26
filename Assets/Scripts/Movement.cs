3f1258bd3553a8a0d3c59d1283c21d71ea904908
10Soumyajit <10soumyajit@gmail.com>
2025-05-26 23:33:44 +0530

    done

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,60 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float Mainthrust = 100f;
-    [SerializeField] float rotationThrust = 1f;
-    [SerializeField] AudioClip mainEngine;
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 100f;
+    [SerializeField] float rotationStrength = 50f;
     Rigidbody rb;
-    AudioSource audioSource;
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+        rotation.Enable();
+    }
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+       }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotateThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotateThisFrame * Time.fixedDeltaTime);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         ProcessThrust();
         ProcessRotation();
-    }
-
-    void ProcessThrust()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddRelativeForce(Vector3.up * Mainthrust * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(mainEngine);
-            }
-        }
-        else
-        {
-            audioSource.Stop();
-        }
-    }
-
-    void ProcessRotation()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            ApplyRotation(rotationThrust);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            ApplyRotation(-rotationThrust);
-        }
-    }
-
-    private void ApplyRotation(float rotationThisFrame)
-    {
-        rb.freezeRotation = true;
-        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
-        rb.freezeRotation = false;
     }
 
 }
